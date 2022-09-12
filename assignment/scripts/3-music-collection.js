ae3b85a984649c4cf7465d9d1b04ad2d3fd3a214
Crystal Redmon <cato2848@gmail.com>
2022-09-12 13:40:01 -0500

    created and tested findByArtist function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,17 +12,17 @@ function addToCollection(title, artist, yearPublished){
 }
 
 
-console.log(addToCollection("Jill Scott", "Who is Jill Scott? Words and Sound, Vol. 1", 2000));
+console.log(addToCollection( "Who is Jill Scott? Words and Sound, Vol. 1", "Jill Scott", 2000));
 
-console.log(addToCollection("Jill Scott", "Beautifully Human: Words and Sounds, Vol. 2", 2004));
+console.log(addToCollection( "Beautifully Human: Words and Sounds, Vol. 2", "Jill Scott", 2004));
 
-console.log(addToCollection("Kanye West", "College Dropout", 2004));
+console.log(addToCollection( "College Dropout", "Kanye West", 2004));
 
-console.log(addToCollection("Gil Scot-Heron", "Pieces of a Man", 1971));
+console.log(addToCollection( "Pieces of a Man", "Gil Scot-Heron", 1971));
 
-console.log(addToCollection("Tom Misch", "Beyond the Groove", 2016));
+console.log(addToCollection( "Beyond the Groove", "Tom Misch", 2016));
 
-console.log(addToCollection("Marvin Gaye", "What's Going On", 1971));
+console.log(addToCollection( "What's Going On", "Marvin Gaye", 1971));
 
 console.log(collection);
 
@@ -34,3 +34,17 @@ function showCollection(array){
 }
 
 showCollection(collection);
+
+
+function findByArtist(artist){
+    let matchingArtist = [];
+    for(let album of collection){
+        if(album.artist === artist){
+            matchingArtist.push(album);
+        }
+    }return matchingArtist;
+};
+
+console.log(findByArtist("Kanye West"))
+console.log(findByArtist("Tom Hanks"));
+
