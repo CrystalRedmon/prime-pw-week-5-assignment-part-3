01605d1c62d9acbb872c08bcf93a694cf4056a90
Crystal Redmon <cato2848@gmail.com>
2022-09-12 12:50:32 -0500

    created addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,15 @@
 console.log('***** Music Collection *****')
 let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+    let newRecord={
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }
+    collection.push(newRecord);
+    return newRecord;
+}
+
+addToCollection("Blah", "Gwen", "2012");
+console.log(collection);
