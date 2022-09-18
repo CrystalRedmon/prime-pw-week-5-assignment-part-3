e16cef535155236ce20d742d4cb059b6cae52df3
Crystal Redmon <cato2848@gmail.com>
2022-09-18 15:36:54 -0500

    completed search function and rewrote showCollection function to include tracks in proper format

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -20,20 +20,31 @@ console.log(addToCollection( "College Dropout", "Kanye West", 2004, [{'Never Let
 console.log(addToCollection( "Pieces of a Man", "Gil Scot-Heron", 1971, [{'Pieces of a Man':'4:55'}, {'Home is Where the Hatred Is': '3:21'}]));
 console.log(addToCollection( "Geography", "Tom Misch", 2018, [{'Movie': '5:57'}, {'Water Baby':'4:31'}]));
 console.log(addToCollection( "What's Going On", "Marvin Gaye", 1971, [{"What's Going On": '3:48'}, {'Mercy, Mercy': '3:03'}]));
-console.log(addToCollection( "Hey Now", "Me", 1971));
+
 console.log(collection);
 
 
 
 /*
+Update the `showCollection` function to display the list of tracks for each album with its name and duration.
+    TITLE by ARTIST, published in YEAR:
+    1. NAME: DURATION
+    2. NAME: DURATION
+    3. NAME: DURATION
+*/
 
 function showCollection(array){
     console.log(array.length);
-    for(album of array){
-        console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished} `)
+    for(let album =0; album< array.length; album++){
+        console.log(array[album].title, 'by', array[album].artist, 'published in', array[album].yearPublished.toString(), ':\n')
+        for(let songNum =1; songNum <= array[album].tracks[0].length; songNum++){
+            let songName = Object.entries(array[album].tracks[0][songNum-1]).join()
+           
+            console.log(songNum + ': '+ songName.replace(',', ': ').toString());
+
         }
         }
-    
+}
 
 
 showCollection(collection);
@@ -41,16 +52,11 @@ showCollection(collection);
 
 
 
-//`${album.title} by ${album.artist}, published in ${album.yearPublished} `
-//(i + ".", album.title, "by", album.artist, "published in", album.yearPublished, )
-
-
 
 function findByArtist(artist){
     let matchingArtist = [];
     for(let album of collection){
         if(album.artist === artist){
-            console.log("Found", album, "in the collection")
             matchingArtist.push(album);
         }
     }return matchingArtist;
@@ -63,23 +69,13 @@ console.log("Expect empty array:", findByArtist("Tom Hanks"));
 findByArtist("Tom Hanks")
 
 
-*/
+
 
 console.log("////////Stretch Goals////////")
 
-//How to access tracks
-//collection[i].tracks[0][i]
-
-console.log(collection[2].tracks[0][0])
-console.log('Spaceship' in collection[2].tracks[0][1])///////On the right track, replace 1 with i
-//('Spaceship' in collection[2].tracks[0][1])
-//Must go down 4 levels
-
-
-
 
 function search(searchBy){
-    if(!searchBy){
+    if(!searchBy || Object.keys(searchBy).length === 0){
         return collection; 
     }
     let matchedCriteria =[];
@@ -87,7 +83,7 @@ for( let i=0; i<collection.length; i++){
     if(collection[i].artist === searchBy.artist && collection[i].yearPublished === searchBy.year){
         for(let j=0; j < collection[i].tracks[0].length; j++){
             if(collection[i].tracks[0][j].hasOwnProperty(searchBy.tracks)){
-            console.log('it works')
+            //console.log('it works')
             matchedCriteria.push(collection[i])
             }
         }
@@ -101,17 +97,13 @@ console.log("Expect album:", search({ artist: 'Jill Scott', year: 2004, tracks:'
 console.log("Expect album:", search({ artist: 'Marvin Gaye', year: 1971, tracks:"What's Going On"}));//matching *all* of the search criteria, result array
 console.log("Expect empty array:", search({ artist: 'Jill Scott', year: 2005 })); //If no results are found, return an empty array.
 console.log("Expect empty array", search({ artist: 'Jill Scott' }));//If no results are found, return an empty array.
-console.log("Expect collection:", search({ })); ///Empty search object, return collection
+console.log("Expect collection:", search({})); ///Empty search object, return collection
 console.log("Expect collection:", search());//No search object, return collection
 
 
 
                         
-//console.log(collection[2].tracks[0]); 
-///This is how to access the tracks array. Must iterate through this to find key name that matches the song criteria. 
 
-//matchedCriteria.push(album); 
-//&& album.tracks === searchBy.tracks
 
 
 
